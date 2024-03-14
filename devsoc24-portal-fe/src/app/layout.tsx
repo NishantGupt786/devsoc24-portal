c505df5912b274283359bb10448ab3494e9cfb25
Abh1noob <abhinavganeshank@gmail.com>
2024-03-14 15:01:58 +0530

    test banner

--- devsoc24-portal-fe/src/app/layout.tsx
+++ devsoc24-portal-fe/src/app/layout.tsx
@@ -39,6 +39,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`font-sans ${inter.className}`}>
+        <div className="w-screen bg-[#2463EB] h-auto flex items-center justify-center font-bold">*For Internal Testing Purposes Only*</div>
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
