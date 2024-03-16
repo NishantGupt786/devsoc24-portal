958223e56aef8232d56ff3a43dd4a3306ab6584a
Abh1noob <abhinavganeshank@gmail.com>
2024-03-16 10:51:11 +0530

    changes

--- devsoc24-portal-fe/src/app/submit-idea/page.tsx
+++ devsoc24-portal-fe/src/app/submit-idea/page.tsx
@@ -6,39 +6,28 @@ import Dashtitle from "@/assets/images/titleDashboard.svg";
 import SubmitIdeaForm from "./submit-idea-form";
 import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Page() {
+  const [screenWidth, setScreenWidth] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth < 1024);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <>
-      <main className="flex h-[200%] flex-col items-start overflow-x-hidden bg-[#F4F5FA] min-[931px]:h-[100vh] min-[931px]:min-h-screen">
-        {/* OG CODE */}
-        {/* <div className="flex flex-row gap-4 lg:gap-8 bg-white w-full">
-          <Logo className="h-9/10 w-auto scale-[0.75] lg:scale-[1] " />
-          <Image
-            src={Dashtitle as HTMLImageElement}
-            alt="title"
-            className="w-[30vw] lg:w-auto"
-          />
-        </div>
-        <div className="flex h-[100vh] w-[4.7rem] items-start justify-center gap-x-8  bg-background px-6 py-2 pt-12 max-[445px]:w-[3.7rem]">
-          <Link href="/home">
-            <ArrowLeft className="text-[#0019FF]" />
-          </Link>
-        </div>
-        <div className="absolute ml-[4.7rem] mt-[11vh] flex w-[91.9vw] flex-col bg-[#F4F5FA] pl-5 pt-5 max-[931px]:justify-center max-[445px]:ml-[3.7rem]  ">
-          <p className="mb-4 text-4xl font-medium text-black">
-            Idea Submission For Devsoc24
-          </p>
-          <SubmitIdeaForm />
-        </div> */}
-
-        {/* I Like this one */}
-        <div className="flex flex-col">
-          <div className="flex h-[8vh] w-full flex-row justify-center gap-4 bg-white  lg:gap-8 px-6">
-            <Link href="/home" className="self-center fixed left-10">
-              <ArrowLeft className="text-[#0019FF]" />
-            </Link>
-            <div className="flex flex-row items-center">
+      {/* OG CODE */}
+      {!screenWidth ? (
+        <>
+          <main className="flex h-[200%] flex-col items-start overflow-x-hidden bg-[#F4F5FA] min-[931px]:h-[100vh] min-[931px]:min-h-screen">
+            <div className="flex w-full flex-row gap-4 bg-white lg:gap-8">
               <Logo className="h-9/10 w-auto scale-[0.75] lg:scale-[1] " />
               <Image
                 src={Dashtitle as HTMLImageElement}
@@ -46,18 +35,50 @@ export default function Page() {
                 className="w-[30vw] lg:w-auto"
               />
             </div>
-          </div>
-          <div className="">
-            <p className="mb-4 text-4xl font-medium text-black text-center m-4 md:my-12">
-              Idea Submission For Devsoc24
-            </p>
-            <div className="">
+            <div className="flex h-[100vh] w-[4.7rem] items-start justify-center gap-x-8  bg-background px-6 py-2 pt-12 max-[445px]:w-[3.7rem]">
+              <Link href="/home">
+                <ArrowLeft className="text-[#0019FF]" />
+              </Link>
+            </div>
+            <div className="absolute ml-[4.7rem] mt-[11vh] flex w-[91.9vw] flex-col bg-[#F4F5FA] pl-5 pt-5 max-[931px]:justify-center max-[445px]:ml-[3.7rem]  ">
+              <p className="mb-4 text-4xl font-medium text-black">
+                Idea Submission For Devsoc24
+              </p>
+
               <SubmitIdeaForm />
             </div>
-          </div>
-        </div>
+          </main>
+        </>
+      ) : (
+        <>
 
-      </main>
+            <div className="flex flex-col bg-">
+              <div className="flex h-[8vh] w-full flex-row justify-center gap-4 bg-white  px-6 lg:gap-8">
+                <Link href="/home" className="fixed left-10 self-center">
+                  <ArrowLeft className="text-[#0019FF]" />
+                </Link>
+                <div className="flex flex-row items-center">
+                  <Logo className="h-9/10 w-auto scale-[0.75] lg:scale-[1] " />
+                  <Image
+                    src={Dashtitle as HTMLImageElement}
+                    alt="title"
+                    className="w-[30vw] lg:w-auto"
+                  />
+                </div>
+              </div>
+              <div className="">
+                <p className="m-4 mb-4 text-center text-4xl font-medium text-black md:my-12">
+                  Idea Submission For Devsoc24
+                </p>
+                <div className="flex w-full items-center justify-center ">
+                  <SubmitIdeaForm />
+                </div>
+              </div>
+            </div>
+        </>
+      )}
+
+      {/* I Like this one */}
     </>
   );
 }
--- devsoc24-portal-fe/src/app/submit-idea/submit-idea-form.tsx
+++ devsoc24-portal-fe/src/app/submit-idea/submit-idea-form.tsx
@@ -100,224 +100,226 @@ export default function SubmitIdeaForm() {
       <ToastContainer />
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)}>
-          <div className="flex w-screen items-center justify-center">
-            <div className="flex w-min justify-start gap-16 max-[931px]:flex-col max-[931px]:gap-6">
-              <div className="flex w-96 flex-col gap-6 max-[445px]:w-[87vw] ">
-                <div>
-                  <FormField
-                    control={form.control}
-                    name="title"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormControl>
-                          <div className="grid w-full max-w-sm items-center gap-4">
-                            <Label
-                              htmlFor="project-name"
-                              className="text-[#0019FF]"
-                            >
-                              Project Name
-                              <span className="text-[#FF0000]">*</span>
-                            </Label>
-                            <Input
-                              type="text"
-                              id="project-name"
-                              {...field}
-                              className={`h-14 bg-white pl-5 ${
-                                form.getFieldState("title").invalid
-                                  ? "border-red-500 focus:border-input focus:!ring-red-500"
-                                  : ""
-                              }`}
-                            />
-                          </div>
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  ></FormField>
-                </div>
-                <div>
-                  <FormField
-                    control={form.control}
-                    name="track"
-                    render={({}) => (
-                      <Controller
-                        name="track"
-                        control={form.control}
-                        render={({ field }) => (
-                          <FormItem>
-                            <FormControl>
-                              <div className="grid w-full max-w-sm items-center gap-4">
-                                <Label
-                                  htmlFor="project-track"
-                                  className="text-[#0019FF] "
-                                >
-                                  Project Track
-                                  <span className="text-[#FF0000]">*</span>
-                                </Label>
-                                <select
-                                  value={form.watch("track")}
-                                  onChange={(e) =>
-                                    form.setValue("track", e.target.value)
-                                  }
-                                  className="rounded-md border border-gray-200 p-4 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-600"
-                                >
-                                  <option value="" disabled selected hidden>
-                                    Select Project Track
-                                  </option>
-                                  {tracks.map((track) => (
-                                    <option key={track} value={track}>
-                                      {track}
+          <div className="flex w-fit flex-col items-center">
+            <div className="flex  items-center justify-center ">
+              <div className="flex w-min justify-start gap-16 max-[931px]:flex-col max-[931px]:gap-6">
+                <div className="flex w-96 flex-col gap-6 max-[445px]:w-[87vw] ">
+                  <div>
+                    <FormField
+                      control={form.control}
+                      name="title"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormControl>
+                            <div className="grid w-full max-w-sm items-center gap-4">
+                              <Label
+                                htmlFor="project-name"
+                                className="text-[#0019FF]"
+                              >
+                                Project Name
+                                <span className="text-[#FF0000]">*</span>
+                              </Label>
+                              <Input
+                                type="text"
+                                id="project-name"
+                                {...field}
+                                className={`h-14 bg-white pl-5 ${
+                                  form.getFieldState("title").invalid
+                                    ? "border-red-500 focus:border-input focus:!ring-red-500"
+                                    : ""
+                                }`}
+                              />
+                            </div>
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    ></FormField>
+                  </div>
+                  <div>
+                    <FormField
+                      control={form.control}
+                      name="track"
+                      render={({}) => (
+                        <Controller
+                          name="track"
+                          control={form.control}
+                          render={({ field }) => (
+                            <FormItem>
+                              <FormControl>
+                                <div className="grid w-full max-w-sm items-center gap-4">
+                                  <Label
+                                    htmlFor="project-track"
+                                    className="text-[#0019FF] "
+                                  >
+                                    Project Track
+                                    <span className="text-[#FF0000]">*</span>
+                                  </Label>
+                                  <select
+                                    value={form.watch("track")}
+                                    onChange={(e) =>
+                                      form.setValue("track", e.target.value)
+                                    }
+                                    className="rounded-md border border-gray-200 p-4 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-600"
+                                  >
+                                    <option value="" disabled selected hidden>
+                                      Select Project Track
                                     </option>
-                                  ))}
-                                </select>
-                              </div>
-                            </FormControl>
-                            <FormMessage />
-                          </FormItem>
-                        )}
-                      />
-                    )}
-                  ></FormField>
-                </div>
-                <div>
-                  <FormField
-                    control={form.control}
-                    name="description"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormControl>
-                          <div className="grid w-full max-w-sm items-center gap-4">
-                            <Label
-                              htmlFor="description"
-                              className="text-[#0019FF]"
-                            >
-                              Description of Project
-                              <span className="text-[#FF0000]">*</span>
-                            </Label>
+                                    {tracks.map((track) => (
+                                      <option key={track} value={track}>
+                                        {track}
+                                      </option>
+                                    ))}
+                                  </select>
+                                </div>
+                              </FormControl>
+                              <FormMessage />
+                            </FormItem>
+                          )}
+                        />
+                      )}
+                    ></FormField>
+                  </div>
+                  <div>
+                    <FormField
+                      control={form.control}
+                      name="description"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormControl>
+                            <div className="grid w-full max-w-sm items-center gap-4">
+                              <Label
+                                htmlFor="description"
+                                className="text-[#0019FF]"
+                              >
+                                Description of Project
+                                <span className="text-[#FF0000]">*</span>
+                              </Label>
 
-                            <Textarea
-                              id="description"
-                              {...field}
-                              placeholder="Don't forget to include your inspiration, learnings, project construction method, and difficulties you encountered in your writing. "
-                              className="col-span-12 mb-6 max-h-72 min-h-40 pl-5 text-black md:col-span-6 md:mb-0"
-                            />
-                          </div>
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  ></FormField>
+                              <Textarea
+                                id="description"
+                                {...field}
+                                placeholder="Don't forget to include your inspiration, learnings, project construction method, and difficulties you encountered in your writing. "
+                                className="col-span-12 mb-6 max-h-72 min-h-40 pl-5 text-black md:col-span-6 md:mb-0"
+                              />
+                            </div>
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    ></FormField>
 
-                  <p className="pr-2 text-right">2500 characters max</p>
+                    <p className="pr-2 text-right">2500 characters max</p>
+                  </div>
                 </div>
-              </div>
-              <div className="flex w-96 flex-col gap-6 max-[445px]:w-[87vw]">
-                <div>
-                  <FormField
-                    control={form.control}
-                    name="figma_link"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormControl>
-                          <div className="grid w-full max-w-sm items-center gap-4">
-                            <Label
-                              htmlFor="figmaLink"
-                              className="text-[#0019FF]"
-                            >
-                              Figma Link
-                            </Label>
-                            <Input
-                              type="text"
-                              id="figmaLink"
-                              {...field}
-                              className={`h-14 bg-white pl-5 ${
-                                form.getFieldState("figma_link").invalid
-                                  ? "border-red-500 focus:border-input focus:!ring-red-500"
-                                  : ""
-                              }`}
-                            />
-                          </div>
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  ></FormField>
-                </div>
-                <div>
-                  <FormField
-                    control={form.control}
-                    name="github_link"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormControl>
-                          <div className="grid w-full max-w-sm items-center gap-4">
-                            <Label
-                              htmlFor="githubLink"
-                              className="text-[#0019FF]"
-                            >
-                              GitHub Link
-                            </Label>
-                            <Input
-                              type="text"
-                              id="githubLink"
-                              {...field}
-                              className={`h-14 bg-white pl-5 ${
-                                form.getFieldState("github_link").invalid
-                                  ? "border-red-500 focus:border-input focus:!ring-red-500"
-                                  : ""
-                              }`}
-                            />
-                          </div>
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  ></FormField>
-                </div>
-                <div>
-                  <FormField
-                    control={form.control}
-                    name="others"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormControl>
-                          <div className="grid w-full max-w-sm items-center gap-4">
-                            <Label
-                              htmlFor="otherLinks"
-                              className="text-[#0019FF]"
-                            >
-                              Other Links
-                            </Label>
+                <div className="flex w-96 flex-col gap-6 max-[445px]:w-[87vw]">
+                  <div>
+                    <FormField
+                      control={form.control}
+                      name="figma_link"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormControl>
+                            <div className="grid w-full max-w-sm items-center gap-4">
+                              <Label
+                                htmlFor="figmaLink"
+                                className="text-[#0019FF]"
+                              >
+                                Figma Link
+                              </Label>
+                              <Input
+                                type="text"
+                                id="figmaLink"
+                                {...field}
+                                className={`h-14 bg-white pl-5 ${
+                                  form.getFieldState("figma_link").invalid
+                                    ? "border-red-500 focus:border-input focus:!ring-red-500"
+                                    : ""
+                                }`}
+                              />
+                            </div>
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    ></FormField>
+                  </div>
+                  <div>
+                    <FormField
+                      control={form.control}
+                      name="github_link"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormControl>
+                            <div className="grid w-full max-w-sm items-center gap-4">
+                              <Label
+                                htmlFor="githubLink"
+                                className="text-[#0019FF]"
+                              >
+                                GitHub Link
+                              </Label>
+                              <Input
+                                type="text"
+                                id="githubLink"
+                                {...field}
+                                className={`h-14 bg-white pl-5 ${
+                                  form.getFieldState("github_link").invalid
+                                    ? "border-red-500 focus:border-input focus:!ring-red-500"
+                                    : ""
+                                }`}
+                              />
+                            </div>
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    ></FormField>
+                  </div>
+                  <div>
+                    <FormField
+                      control={form.control}
+                      name="others"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormControl>
+                            <div className="grid w-full max-w-sm items-center gap-4">
+                              <Label
+                                htmlFor="otherLinks"
+                                className="text-[#0019FF]"
+                              >
+                                Other Links
+                              </Label>
 
-                            <Textarea
-                              id="otherLinks"
-                              {...field}
-                              className="col-span-12 mb-6 min-h-10 pl-5 text-black md:col-span-6 md:mb-0"
-                            />
-                          </div>
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  ></FormField>
+                              <Textarea
+                                id="otherLinks"
+                                {...field}
+                                className="col-span-12 mb-6 min-h-10 pl-5 text-black md:col-span-6 md:mb-0"
+                              />
+                            </div>
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    ></FormField>
 
-                  <p>
-                    Canva PPTs, Videos, Drive or Supporting Material can be
-                    shared
-                  </p>
+                    <p>
+                      Canva PPTs, Videos, Drive or Supporting Material can be
+                      shared
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="flex w-screen items-center justify-center">
-            <Button
-              className="my-5 bg-[#0019FF]"
-              type="submit"
-              //   disabled={isSubmitting}
-            >
-              <Image src={send as HTMLImageElement} alt="b" />
-              <span className="pl-2">Submit Idea</span>
-            </Button>
+            <div className="">
+              <Button
+                className="my-5 bg-[#0019FF]"
+                type="submit"
+                //   disabled={isSubmitting}
+              >
+                <Image src={send as HTMLImageElement} alt="b" />
+                <span className="pl-2">Submit Idea</span>
+              </Button>
+            </div>
           </div>
         </form>
       </Form>
