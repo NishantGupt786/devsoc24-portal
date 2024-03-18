c92d240056d9c2500db050d8cbc83845b9a5b173
s176i <saanvi.iyer007@gmail.com>
2024-03-18 23:38:37 +0530

    feat: timeline changes

--- devsoc24-portal-fe/src/app/home/page.tsx
+++ devsoc24-portal-fe/src/app/home/page.tsx
@@ -446,7 +446,7 @@ export default function HomePage() {
           {showModal === "leave" && <LeaveTeam />}
           {showModal === "kick" && <Kick />}
           <div className="mt-4 flex h-fit w-screen flex-col justify-between gap-4 px-4">
-            <TimelineComponent count={5} />
+            <TimelineComponent count={6} />
           </div>
 
           <div className="mt-4 flex h-fit w-screen flex-col justify-between gap-4 px-4 md:flex-row lg:h-[85%]">
